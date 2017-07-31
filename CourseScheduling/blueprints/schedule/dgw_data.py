36859a6131e0c615b6630b1d0de6ee1fe5c829ad
Yitong Wu <yitongw2@uci.edu>
2017-07-30 17:45:55 -0700

    parse course.xml for taken courses

--- CourseScheduling/blueprints/schedule/dgw_data.py
+++ CourseScheduling/blueprints/schedule/dgw_data.py
@@ -8,15 +8,17 @@ class data:
 		self.cookies = dict()
 		self.name = None
 		self.studentID = None
-		self.major = None
+		self.major = list()
+		self.minor = list()
+		self.spec = list()
+		self.applied_classes = list()
 		self.degree = None
 		self.level = None
-		self.spec = None
 		self.process_cookies(cookies)
 
 	def getDict(self):
 		return {'name':self.name, 'id':self.studentID, 'major':self.major, 'degree':self.degree,
-			'level':self.level, 'spec':self.spec}
+			'level':self.level, 'spec':self.spec, 'minor':self.minor, 'taken': self.applied_classes}
 
 	def process_cookies(self, cookies):
 		for key_val in cookies.split(';'):
@@ -50,28 +52,14 @@ class data:
 		goaldtl = stu_data.goaldtl
 		self.degree = goaldtl['degree']
 		self.level = goaldtl['stulevel']
-
-		for gdt in stu_data.find_all("goaldatadtl"):
-			if gdt['goalcode'].lower() == 'major':
-				self.major = gdt['goalvalue']
-			elif gdt['goalcode'].lower() == 'spec':
-				self.spec = gdt['goalvalue']
 		
 		name_pattern = 'sILStudentName = \"(.+)\";'
 		rf = re.search(name_pattern, r.text)
 		if rf:
 			self.name = rf.group(1).strip()
 
-		major_pattern = 'sMajorPicklist\[sMajorPicklist.length\] = new DataItem\(\".*%s.*\", \"(.+)\"\);' % (self.major)
-		rf = re.search(major_pattern, r.text)
-		if rf:
-			self.major = rf.group(1).strip()
-		else:
-			self.major = None
-
 		level_pattern = 'sLevelPicklist\[sLevelPicklist.length\] = new DataItem\(\".*%s.*\", \"(.+)\"\);' % (self.level)
 		rf = re.search(level_pattern, r.text)
-		print (rf)
 		if rf:
 			self.level = rf.group(1).strip()
 		else:
@@ -83,5 +71,36 @@ class data:
 
 		with open('courses.xml', 'w') as f:
 			f.write(r.text)
+
+		soup = BeautifulSoup(r.text, 'lxml')
+		for goal in soup.find('deginfo').findAll('goal'):
+			if goal['code'].lower() == 'major':
+				self.major.append(goal['valuelit'])
+			elif goal['code'].lower() == 'minor':
+				self.minor.append(goal['valuelit'])
+			elif goal['code'].lower() == 'spec':
+				self.spec.append(goal['valuelit'])
+
+		for applied_classes in soup.findAll('classesapplied'):
+			for cla in applied_classes.findAll('class'):
+				disc, num = '', ''
+				if len(cla.get('disc', '')) > 0:
+					disc = cla['disc']
+				elif len(cla.get('discipline', '')) > 0:
+					disc = cla['discipline']
+
+				if len(cla.get('num', '')) > 0:
+					num = cla['num']
+				elif len(cla.get('number', '')) > 0:
+					num = cla['number']
+
+				if len(disc) > 0 and len(num) > 0:
+					self.applied_classes.append(disc + ' ' + num)
+
+
+
+
+
+
 		
 
